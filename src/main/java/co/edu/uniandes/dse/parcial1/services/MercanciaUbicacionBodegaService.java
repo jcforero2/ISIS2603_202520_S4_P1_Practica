5188f04f8280cf94710395af2402859255703bdd
jcforero2 <jc.forero2@uniandes.edu.co>
2025-09-17 09:19:07 -0500

    Ultimo avance del parcial

--- src/main/java/co/edu/uniandes/dse/parcial1/services/MercanciaUbicacionBodegaService.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/services/MercanciaUbicacionBodegaService.java
@@ -3,8 +3,11 @@ package co.edu.uniandes.dse.parcial1.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.uniandes.dse.parcial1.entities.MercanciaEntity;
+import co.edu.uniandes.dse.parcial1.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.parcial1.repositories.MercanciaRepository;
 import co.edu.uniandes.dse.parcial1.repositories.UbicacionBodegaRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,4 +20,9 @@ public class MercanciaUbicacionBodegaService {
     @Autowired
     private UbicacionBodegaRepository ubicacionbodegaRepository;
 
+
+    @Transactional
+    public MercanciaEntity assignubicacionbodega(Long mercanciaID, Long ubicacionbodegaID) throws IllegalOperationException{
+        
+    }
 }
